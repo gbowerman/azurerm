31d433cb8758777e4cc7878541bc60682940b8d5
Guy Bowerman <guybo@pondlife.redmond.corp.microsoft.com>
2016-02-15 01:10:12 -0800

    Minor version/description update

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(name='azurerm',
-      version='0.4',
-      description='Simple Azure Resource Manager library',
+      version='0.4.1',
+      description='Azure Resource Manager REST wrappers',
       url='http://github.com/gbowerman/azurerm',
       author='sendmarsh',
       author_email='guybo@outlook.com',
       license='MIT',
       packages=['azurerm'],
-	  install_requires=[
+      install_requires=[
           'adal',
+          'requests',
       ],
       zip_safe=False)
