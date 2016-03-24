2c8cc2d649c66461a966546af0d28d70466675f6
Guy Bowerman <guybo@microsoft.com>
2016-03-24 16:57:13 -0700

    Latest version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='azurerm',
-      version='0.4.4',
+      version='0.4.5',
       description='Azure Resource Manager REST wrappers',
       url='http://github.com/gbowerman/azurerm',
       author='sendmarsh',
