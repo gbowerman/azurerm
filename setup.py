57e1c5681bd43d0f37df89239d552427a237f595
gbowerman <guybo@outlook.com>
2016-09-03 09:56:03 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='azurerm',
-      version='0.5.9',
+      version='0.6.0',
       description='Azure Resource Manager REST wrappers',
       url='http://github.com/gbowerman/azurerm',
       author='sendmarsh',
