1a941f9b3ca0c45e4f3a9aa677ce4d9d5c76b865
gbowerman <guybo@outlook.com>
2016-09-19 16:35:57 -0700

    Update INSIGHTS_API and azurerm version

--- azurerm/settings.py
+++ azurerm/settings.py
@@ -4,7 +4,7 @@ azure_rm_endpoint = 'https://management.azure.com'
 
 BASE_API = '2015-01-01'
 COMP_API = '2016-03-30'
-INSIGHTS_API = '2014-04-01'
+INSIGHTS_API = '2016-03-01'
 INSIGHTS_PREVIEW_API = '2016-06-01'
 MEDIA_API = '2015-10-01'
 NETWORK_API = '2016-06-01'
