05eb58151ba8bef4da1c901f2d722d809915376f
negat <negat@microsoft.com>
2016-09-19 21:49:58 +0000

    added list_metrics_for_resource function

--- azurerm/insightsrp.py
+++ azurerm/insightsrp.py
@@ -3,7 +3,6 @@
 from .restfns import do_get
 from .settings import azure_rm_endpoint, INSIGHTS_API
 
-
 # list_autoscale_settings(access_token, subscription_id)
 # list the autoscale settings in a subscription_id
 def list_autoscale_settings(access_token, subscription_id):
@@ -22,4 +21,16 @@ def list_insights_components(access_token, subscription_id, resource_group):
                         '/resourceGroups/', resource_group,
                         '/providers/microsoft.insights/',
                         '/components?api-version=', INSIGHTS_API])
+    return do_get(endpoint, access_token)
+
+
+def list_metrics_for_resource(access_token, subscription_id, resource_group, resource_provider, resource_type, resource_name):
+    endpoint = ''.join([azure_rm_endpoint,
+                        '/subscriptions/', subscription_id,
+                        '/resourceGroups/', resource_group,
+                        '/providers/', resource_provider,
+                        '/', resource_type,
+                        '/', resource_name,
+                        '/providers/microsoft.insights/',
+                        '/metricdefinitions?api-version=', INSIGHTS_API])
     return do_get(endpoint, access_token)
