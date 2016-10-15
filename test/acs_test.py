bd9400a5c5b4fa7979ca276ac349e2239bf84898
gbowerman <guybo@outlook.com>
2016-10-15 11:19:01 -0700

    Simply crypto

--- test/acs_test.py
+++ test/acs_test.py
@@ -10,9 +10,9 @@ import time
 import os
 
 from os import chmod
-from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from cryptography.hazmat.backends import default_backend
 
 class TestAzurermPy(unittest.TestCase):
 
@@ -38,10 +38,10 @@ class TestAzurermPy(unittest.TestCase):
         self.master_dns = Haikunator.haikunate(delimiter='')
 
         # generate RSA Key for container service
-        key = rsa.generate_private_key(backend=crypto_default_backend(), public_exponent=65537, \
+        key = rsa.generate_private_key(backend=default_backend(), public_exponent=65537, \
             key_size=2048)
-        self.public_key = key.public_key().public_bytes(crypto_serialization.Encoding.OpenSSH, \
-            crypto_serialization.PublicFormat.OpenSSH).decode('utf-8')
+        self.public_key = key.public_key().public_bytes(serialization.Encoding.OpenSSH, \
+            serialization.PublicFormat.OpenSSH).decode('utf-8')
         
         # create resource group
         print('Creating resource group: ' + self.rgname)
