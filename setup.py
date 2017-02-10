3ea764095ec200e1bc59977b6e89d6aae745113c
gbowerman <guybo@outlook.com>
2017-02-10 15:28:03 -0800

    Update Compute API to 2017-03-30

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='azurerm',
-      version='0.7.17',
+      version='0.7.18',
       description='Azure Resource Manager REST wrappers',
       url='http://github.com/gbowerman/azurerm',
       author='sendmarsh',
