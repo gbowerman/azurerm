e83ae8896e4486bbfa513f9cd93db399ffffd3bb
gbowerman <guybo@outlook.com>
2017-03-07 12:31:41 -0800

    list_pubs.py example update

--- examples/list_pubs.py
+++ examples/list_pubs.py
@@ -6,7 +6,7 @@ try:
    with open('azurermconfig.json') as configFile:    
       configData = json.load(configFile)
 except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
+   print("Error: Expecting azurermonfig.json in current folder")
    sys.exit()
    
 tenant_id = configData['tenantId']
@@ -15,17 +15,25 @@ app_secret = configData['appSecret']
 subscription_id = configData['subscriptionId']
 
 access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
-
-#pubs = azurerm.list_publishers(access_token, subscription_id, 'southeastasia')
 '''
-skus = azurerm.list_skus(access_token, subscription_id, 'southeastasia', 'MicrosoftWindowsServer', 'WindowsServer')
+pubs = azurerm.list_publishers(access_token, subscription_id, 'southeastasia')
+for pub in pubs:
+#    print(json.dumps(pub, sort_keys=False, indent=2, separators=(',', ': ')))
+    print(pub['name'])
+
+offers = azurerm.list_offers(access_token, subscription_id, 'southeastasia', 'rancher')
+for offer in offers:
+    print(json.dumps(offer, sort_keys=False, indent=2, separators=(',', ': ')))
+
+#skus = azurerm.list_skus(access_token, subscription_id, 'southeastasia', 'MicrosoftWindowsServer', 'WindowsServer')
 # skus = azurerm.list_skus(access_token, subscription_id, 'southeastasia', 'Canonical', 'UbuntuServer')
+skus = azurerm.list_skus(access_token, subscription_id, 'southeastasia', 'rancher', 'rancheros')
 for sku in skus:
     print(sku['name'])
 '''
 #print('Versions for CoreOS:')
-#versions = azurerm.list_sku_versions(access_token, subscription_id, 'southeastasia', 'Canonical', 'UbuntuServer', '16.04.0-LTS')
-# versions = azurerm.list_sku_versions(access_token, subscription_id, 'southeastasia', 'Canonical', 'UbuntuServer', '15.10')
-versions = azurerm.list_sku_versions(access_token, subscription_id, 'eastasia', 'CoreOS', 'CoreOS', 'Stable')
+versions = azurerm.list_sku_versions(access_token, subscription_id, 'southeastasia', 'Canonical', 'UbuntuServer', '16.04-LTS')
+#versions = azurerm.list_sku_versions(access_token, subscription_id, 'eastasia', 'CoreOS', 'CoreOS', 'Stable')
+# versions = azurerm.list_sku_versions(access_token, subscription_id, 'eastasia', 'rancher', 'rancheros', 'os')
 for version in versions:
     print(version['name'])
