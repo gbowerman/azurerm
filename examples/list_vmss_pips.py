184cc6448a7bed4c945b0c5cb1e3739c3fb9c7f8
Guy Bowerman <guybo@outlook.com>
2017-05-05 16:03:33 -0700

    Improve list VMSS public IP example

--- examples/list_vmss_pips.py
+++ examples/list_vmss_pips.py
@@ -1,13 +1,25 @@
+import argparse
 import azurerm
 import json
+import re
 import sys
 
-# check for single command argument    
-if len(sys.argv) == 3:
-    rg = sys.argv[1]
-    vmss = sys.argv[2]
-else:
-      sys.exit('Expecting resource group name and vmss name as arguments.') 
+# validate command line arguments
+argParser = argparse.ArgumentParser()
+
+argParser.add_argument('--vmssname', '-n', required=True,
+                       action='store', help='VMSS Name')
+argParser.add_argument('--rgname', '-g', required=True,
+                       action='store', help='Resource Group Name')
+argParser.add_argument('--details', '-a', required=False,
+                       action='store', help='Print all details')
+
+args = argParser.parse_args()
+
+name = args.vmssname
+rgname = args.rgname
+details = args.details
+
 # Load Azure app defaults
 try:
     with open('azurermconfig.json') as configFile:
@@ -23,6 +35,13 @@ subscription_id = configData['subscriptionId']
 
 access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
-public_ips = azurerm.get_vmss_public_ips(access_token, subscription_id, rg, vmss)
-print(json.dumps(public_ips, sort_keys=False, indent=2, separators=(',', ': ')))
+public_ips = azurerm.get_vmss_public_ips(access_token, subscription_id, rgname, name)
+
+if details is True:
+    print(json.dumps(public_ips, sort_keys=False, indent=2, separators=(',', ': ')))
+else:
+    for ip in public_ips['value']:
+        vm_id = re.search('Machines/(.*)/networkInt', ip['id']).group(1)
+        ipaddr = ip['properties']['ipAddress']
+        print('VM id: ' + vm_id + ', IP: ' + ipaddr)
 
