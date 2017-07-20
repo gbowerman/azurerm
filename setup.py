4f18f83f6b29cf2da296c03b4dd9cf657a4e2108
Guy Bowerman <guybo@outlook.com>
2017-07-20 11:01:12 -0700

    Import README to pyi

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
 setup(name='azurerm',
-      version='0.8.2',
+      version='0.8.6',
       description='Azure Resource Manager REST wrappers',
+      long_description=long_description,
       url='http://github.com/gbowerman/azurerm',
       author='sendmarsh',
       author_email='guybo@outlook.com',
