84a2291756e140d658ed0c30240f7c608cec86f5
Guy Bowerman <guybo@outlook.com>
2017-08-06 14:48:35 -0700

    Documentation strings for adalfns module

--- azurerm/adalfns.py
+++ azurerm/adalfns.py
@@ -1,4 +1,4 @@
-# adalfns - place to store azurerm functions which call adal routines
+''' adalfns - place to store azurerm functions which call adal routines'''
 import json
 import os
 from datetime import datetime as dt
@@ -8,9 +8,17 @@ import adal
 from .settings import get_auth_endpoint, get_resource_endpoint
 
 
-# get_access_token(tenant_id, application_id, application_secret)
-# get an Azure access token using the adal library
 def get_access_token(tenant_id, application_id, application_secret):
+    '''get an Azure access token using the adal library
+
+    Args:
+        tenant_id (str): Tenant id of the user's account.
+        application_id (str): Application id of a Service Principal account.
+        application_secret (str): Application secret (password) of the Service Principal account.
+
+    Returns:
+        An Azure authentication token string.
+    '''
     context = adal.AuthenticationContext(
         get_auth_endpoint() + tenant_id, api_version=None)
     token_response = context.acquire_token_with_client_credentials(
@@ -18,11 +26,15 @@ def get_access_token(tenant_id, application_id, application_secret):
     return token_response.get('accessToken')
 
 
-# get_access_token_from_cli()
-# get an Azure authentication token from CLI's local cache
-# will only work if CLI local cache has an unexpired auth token (i.e. you
-# ran 'az login' recently)
 def get_access_token_from_cli():
+    '''Get an Azure authentication token from CLI's local cache
+
+    Will only work if CLI local cache has an unexpired auth token (i.e. you ran 'az login' 
+        recently)
+
+    Returns:
+        An Azure authentication token string.
+    '''
     home = os.path.expanduser('~')
     access_keys_path = home + os.sep + '.azure' + os.sep + 'accessTokens.json'
     if os.path.isfile(access_keys_path) is False:
