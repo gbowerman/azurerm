debd6076f36daff4c87ffa26760e0cfc7afba83f
Guy Bowerman <guybo@outlook.com>
2017-08-07 08:12:53 -0700

    Update media services functions to pylint docstrings

--- azurerm/amsrp.py
+++ azurerm/amsrp.py
@@ -1,52 +1,93 @@
-# amsrp.py - azurerm functions for the Microsoft.Media resource provider
+'''amsrp.py - azurerm functions for the Microsoft.Media resource provider.'''
 import json
 from .restfns import do_get, do_post, do_put, do_delete
 from .settings import get_rm_endpoint, MEDIA_API
 
 
-# check_name_availability of a media service name(access_token, subscription_id, rgname)
-# check the media service name availability in a rgname and msname
-def check_media_service_name_availability(access_token, subscription_id, name):
+def check_ms_name_availability(access_token, subscription_id, msname):
+    '''Check media service name availability.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        msname (str): media service name.
+
+    Returns:
+        HTTP response.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
-                        '/providers/microsoft.media/CheckNameAvailability?api-version=', MEDIA_API])
-    ms_body = {'name': name}
+                        '/providers/microsoft.media/CheckNameAvailability?',
+                        'api-version=', MEDIA_API])
+    ms_body = {'name': msname}
     ms_body['type'] = 'mediaservices'
     body = json.dumps(ms_body)
     return do_post(endpoint, body, access_token)
 
-# create_media_service_rg(access_token, subscription_id, rgname)
-# create the media service in a rgname
-def create_media_service_rg(access_token, subscription_id, rgname, location, stoname, name):
+
+def create_media_service_rg(access_token, subscription_id, rgname, location, stoname, msname):
+    '''Create a media service in a resource group.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        rgname (str): Azure resource group name.
+        location (str): Azure data center location. E.g. westus.
+        stoname (str): Azure storage account name.
+        msname (str): Media service name.
+
+    Returns:
+        HTTP response. JSON body.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
                         '/resourceGroups/', rgname,
-                        '/providers/microsoft.media/mediaservices/' + name + '?api-version=', MEDIA_API])
-    ms_body = {'name': name}
+                        '/providers/microsoft.media/mediaservices/', msname,
+                        '?api-version=', MEDIA_API])
+    ms_body = {'name': msname}
     ms_body['location'] = location
     sub_id_str = '/subscriptions/' + subscription_id + '/resourceGroups/' + rgname + \
         '/providers/Microsoft.Storage/storageAccounts/' + stoname
     storage_account = {'id': sub_id_str}
     storage_account['isPrimary'] = True
     properties = {'storageAccounts': [storage_account]}
+    ms_body['properties'] = properties
     body = json.dumps(ms_body)
     return do_put(endpoint, body, access_token)
 
 
-# delete_media_service_rg(access_token, subscription_id, rgname)
-# delete the media service in a rgname
-def delete_media_service_rg(access_token, subscription_id, rgname, location, stoname, name):
+def delete_media_service_rg(access_token, subscription_id, rgname, msname):
+    '''Delete a media service.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        rgname (str): Azure resource group name.
+        msname (str): Media service name.
+
+    Returns:
+        HTTP response.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
                         '/resourceGroups/', rgname,
-                        '/providers/microsoft.media/mediaservices/' + name + '?api-version=', MEDIA_API])
-
+                        '/providers/microsoft.media/mediaservices/', msname,
+                        '?api-version=', MEDIA_API])
     return do_delete(endpoint, access_token)
 
 
-# list_media_endpoint_keys in a resrouce group(access_token, subscription_id, rgname, msname)
-# list the media endpoint keys in a rgname and msname
 def list_media_endpoint_keys(access_token, subscription_id, rgname, msname):
+    '''list the media endpoint keys in a media service
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        rgname (str): Azure resource group name.
+        msname (str): Media service name.
+
+    Returns:
+        HTTP response. JSON body.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
                         '/resourceGroups/', rgname,
@@ -56,18 +97,33 @@ def list_media_endpoint_keys(access_token, subscription_id, rgname, msname):
     return do_get(endpoint, access_token)
 
 
-# list_media_services(access_token, subscription_id)
-# list the media services in a subscription_id
 def list_media_services(access_token, subscription_id):
+    '''List the media services in a subscription.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+
+    Returns:
+        HTTP response. JSON body.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
                         '/providers/microsoft.media/mediaservices?api-version=', MEDIA_API])
     return do_get(endpoint, access_token)
 
 
-# list_media_services_rg in a resrouce group(access_token, subscription_id, rgname)
-# list the media services in a rgname
 def list_media_services_rg(access_token, subscription_id, rgname):
+    '''List the media services in a resource group.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        rgname (str): Azure resource group name.
+
+    Returns:
+        HTTP response. JSON body.
+    '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
                         '/resourceGroups/', rgname,
