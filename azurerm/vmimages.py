37306a6f874a82ff84bdba8a48f90753417574e9
Guy Bowerman <guybo@outlook.com>
2017-08-08 15:26:52 -0700

    Finish docstring comments for vmimages.py

--- azurerm/vmimages.py
+++ azurerm/vmimages.py
@@ -11,6 +11,7 @@ def list_offers(access_token, subscription_id, location, publisher):
         access_token (str): A valid Azure authentication token.
         subscription_id (str): Azure subscription id.
         location (str): Azure data center location. E.g. westus.
+        publisher (str): Publisher name, e.g. Canonical.
 
     Returns:
         HTTP response with JSON list of image offers.
@@ -25,7 +26,15 @@ def list_offers(access_token, subscription_id, location, publisher):
 
 
 def list_publishers(access_token, subscription_id, location):
-    '''List available image publishers for a location.
+    '''List available image publishers for a location. E.g. MicrosoftWindowsServer.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        location (str): Azure data center location. E.g. westus.
+
+    Returns:
+        HTTP response with JSON list of publishers.
     '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
@@ -37,6 +46,16 @@ def list_publishers(access_token, subscription_id, location):
 
 def list_skus(access_token, subscription_id, location, publisher, offer):
     '''List available VM image skus for a publisher offer.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        location (str): Azure data center location. E.g. westus.
+        publisher (str): VM image publisher. E.g. MicrosoftWindowsServer.
+        offer (str): VM image offer. E.g. WindowsServer.
+
+    Returns:
+        HTTP response with JSON list of skus.
     '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
@@ -50,6 +69,17 @@ def list_skus(access_token, subscription_id, location, publisher, offer):
 
 def list_sku_versions(access_token, subscription_id, location, publisher, offer, sku):
     '''List available versions for a given publisher's sku.
+
+    Args:
+        access_token (str): A valid Azure authentication token.
+        subscription_id (str): Azure subscription id.
+        location (str): Azure data center location. E.g. westus.
+        publisher (str): VM image publisher. E.g. MicrosoftWindowsServer.
+        offer (str): VM image offer. E.g. WindowsServer.
+        sku (str): VM image sku. E.g. 2016-Datacenter.
+
+    Returns:
+        HTTP response with JSON list of versions.
     '''
     endpoint = ''.join([get_rm_endpoint(),
                         '/subscriptions/', subscription_id,
