ffd2ceafbc9b47067ebf66586eb7610891dc833a
Guy Bowerman <guybo@outlook.com>
2017-08-18 15:09:57 -0700

    Update deployment examples

--- examples/deploytemplatecli.py
+++ examples/deploytemplatecli.py
@@ -1,4 +1,5 @@
-'''deploytemplate.py - deploy a template from a template URI and a local parameters file'''
+'''deploytemplatecli.py - deploys template URI and local params file, authenticates using CLI'''
+
 import argparse
 import json
 import sys
@@ -6,36 +7,25 @@ import sys
 import azurerm
 from haikunator import Haikunator
 
-
 def main():
     '''Main routine.'''
     # validate command line arguments
     arg_parser = argparse.ArgumentParser()
 
-    arg_parser.add_argument('--uri', '-u', required=True,
-                            action='store', help='Template URI')
+    arg_parser.add_argument('--uri', '-u', required=True, action='store', help='Template URI')
     arg_parser.add_argument('--params', '-p', required=True, action='store',
                             help='Parameters json file')
     arg_parser.add_argument('--rg', '-g', required=True, action='store',
                             help='Resource Group name')
+    arg_parser.add_argument('--sub', '-s', required=False, action='store',
+                            help='subscription id (optional)')
+
     args = arg_parser.parse_args()
 
     template_uri = args.uri
     params = args.params
     rgname = args.rg
-
-    # Load Azure app defaults
-    try:
-        with open('azurermconfig.json') as config_file:
-            config_data = json.load(config_file)
-    except FileNotFoundError:
-        print('Error: Expecting azurermconfig.json in current folder')
-        sys.exit()
-
-    tenant_id = config_data['tenantId']
-    app_id = config_data['appId']
-    app_secret = config_data['appSecret']
-    subscription_id = config_data['subscriptionId']
+    subscription_id = args.sub
 
     # load parameters file
     try:
@@ -45,13 +35,16 @@ def main():
         print('Error: Expecting ' + params + ' in current folder')
         sys.exit()
 
-    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+    access_token = azurerm.get_access_token_from_cli()
+    if subscription_id is None:
+        subscription_id = azurerm.get_subscription_from_cli()
     deployment_name = Haikunator().haikunate()
     print('Deployment name:' + deployment_name)
 
-    deploy_return = azurerm.deploy_template_uri(access_token, subscription_id, rgname,
-                                                deployment_name, template_uri, param_data)
-    print(deploy_return)
+    deploy_return = azurerm.deploy_template_uri(
+        access_token, subscription_id, rgname, deployment_name, template_uri, param_data)
+
+    print(json.dumps(deploy_return.json(), sort_keys=False, indent=2, separators=(',', ': ')))
 
 
 if __name__ == "__main__":
