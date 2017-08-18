08a4ec9f5ee47175038ccf3e6d859158d812f113
Guy Bowerman <guybo@outlook.com>
2017-08-18 15:17:25 -0700

    Update get_vmisntanceview.py example

--- examples/get_vminstanceview.py
+++ examples/get_vminstanceview.py
@@ -1,36 +1,46 @@
-import azurerm
+'''get_vminstanceview.py - returns the instance view for an Azure VM'''
 import json
 import sys
 
+import azurerm
+
+
 def usage():
-    sys.exit('Usage: python ' + sys.argv[0] + ' rg_name vm_name [vmid]')
+    '''Return usage and exit.'''
+    sys.exit('Usage: python ' + sys.argv[0] + ' rg_name vm_name')
 
-# process arguments   
-if len(sys.argv) < 3:
-    usage()
 
-rg = sys.argv[1]
-vm = sys.argv[2]
+def main():
+    '''Main routine.'''
+    # process arguments
+    if len(sys.argv) < 3:
+        usage()
 
-# Load Azure app defaults
-try:
-   with open('azurermconfig.json') as configFile:    
-      configData = json.load(configFile)
-except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
-   sys.exit()
+    rgname = sys.argv[1]
+    vm_name = sys.argv[2]
 
-tenant_id = configData['tenantId']
-app_id = configData['appId']
-app_secret = configData['appSecret']
-subscription_id = configData['subscriptionId']
+    # Load Azure app defaults
+    try:
+        with open('azurermconfig.json') as config_file:
+            config_data = json.load(config_file)
+    except FileNotFoundError:
+        sys.exit('Error: Expecting azurermconfig.json in current folder')
 
-access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+    tenant_id = config_data['tenantId']
+    app_id = config_data['appId']
+    app_secret = config_data['appSecret']
+    subscription_id = config_data['subscriptionId']
 
-print('Getting VM instance view\n')
-instance_view = azurerm.get_vm_instance_view(access_token, subscription_id, rg, vm)
-# print(json.dumps(instance_view, sort_keys=False, indent=2, separators=(',', ': ')))
-for status in instance_view['statuses']:
-    print('Code: ' + status['code'])                                   
-    
+    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
+    print('Getting VM instance view\n')
+    instance_view = azurerm.get_vm_instance_view(
+        access_token, subscription_id, rgname, vm_name)
+    print(json.dumps(instance_view, sort_keys=False,
+                     indent=2, separators=(',', ': ')))
+    # for status in instance_view['statuses']:
+    #    print('Code: ' + status['code'])
+
+
+if __name__ == "__main__":
+    main()
