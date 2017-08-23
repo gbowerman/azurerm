f8878f0e1b9eae11270783969bab39468fbbffa9
Guy Bowerman <guybo@outlook.com>
2017-08-22 17:40:54 -0700

    version increment for cosmosdb (@iainfolulds)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 
 setup(name='azurerm',
-      version='0.8.23',
+      version='0.8.24',
       description='Azure Resource Manager REST wrappers',
       long_description=LONG_DESCRIPTION,
       url='http://github.com/gbowerman/azurerm',
