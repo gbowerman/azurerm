a8234b898e25a3f7b28f82e2be04300727e83a9c
Guy Bowerman <guybo@outlook.com>
2017-08-27 15:03:10 -0700

    Fix version

--- examples/instanceviews.py
+++ examples/instanceviews.py
@@ -1,25 +1,34 @@
+'''instanceviews.py - lists VM instance views for a scale set'''
 import azurerm
+import sys
+
 import json
 
-# Load Azure app defaults
-try:
-   with open('azurermconfig.json') as configFile:    
-      configData = json.load(configFile)
-except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
-   sys.exit()
-   
-tenant_id = configData['tenantId']
-app_id = configData['appId']
-app_secret = configData['appSecret']
-subscription_id = configData['subscriptionId']
-rg = configData['resourceGroup']
-vmss = configData['vmssName']
 
-access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+def main():
+    '''Main routine.'''
+    # Load Azure app defaults
+    try:
+       with open('azurermconfig.json') as configFile:
+           configData = json.load(configFile)
+    except FileNotFoundError:
+       sys.exit("Error: Expecting azurermconfig.json in current folder")
 
-# loop through resource groups
-instances = azurerm.list_vmss_vm_instance_view(access_token, subscription_id, rg, vmss)
+    tenant_id = configData['tenantId']
+    app_id = configData['appId']
+    app_secret = configData['appSecret']
+    subscription_id = configData['subscriptionId']
+    rg = configData['resourceGroup']
+    vmss = configData['vmssName']
 
-print(json.dumps(instances, sort_keys=False, indent=2, separators=(',', ': ')))
+    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+
+    # loop through resource groups
+    instances = azurerm.list_vmss_vm_instance_view(access_token, subscription_id, rg, vmss)
+
+    print(json.dumps(instances, sort_keys=False, indent=2, separators=(',', ': ')))
+
+
+if __name__ == "__main__":
+    main()
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 
 setup(name='azurerm',
-      version='0.8.25',
+      version='0.8.26',
       description='Azure Resource Manager REST wrappers',
       long_description=LONG_DESCRIPTION,
       url='http://github.com/gbowerman/azurerm',
