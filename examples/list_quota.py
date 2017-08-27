c267d400962625cd9cd86995622ab88d3cb13bb3
Guy Bowerman <guybo@outlook.com>
2017-08-27 16:01:41 -0700

    Format list_pubs, list_quota, list_regions, list_rgs examples

--- examples/list_quota.py
+++ examples/list_quota.py
@@ -1,50 +1,57 @@
-import azurerm
+'''list_quota.py - list Compute usage quota for specific regions or all'''
 import json
 import sys
 
-Summary = False
+import azurerm
 
-def print_region_quota(region):
+SUMMARY = False
+
+def print_region_quota(access_token, sub_id, region):
+    '''Print the Compute usage quota for a specific region'''
     print(region + ':')
-    quota = azurerm.get_compute_usage(access_token, subscription_id, region)
-    if Summary == False:
+    quota = azurerm.get_compute_usage(access_token, sub_id, region)
+    if SUMMARY is False:
         print(json.dumps(quota, sort_keys=False, indent=2, separators=(',', ': ')))
     try:
         for resource in quota['value']:
             if resource['name']['value'] == 'cores':
-                print('   Current: ' + str(resource['currentValue']) + ', limit: ' + str(resource['limit']))
+                print('   Current: ' + str(resource['currentValue']) + ', limit: '
+                      + str(resource['limit']))
                 break
     except KeyError:
         print('Invalid data for region: ' + region)
 
-# check for single command argument    
-if len(sys.argv) != 2:
-    region = 'all'
-    Summary = True
-else:
-    region = sys.argv[1]
+def main():
+    '''Main routine.'''
+    # check for single command argument
+    if len(sys.argv) != 2:
+        region = 'all'
+    else:
+        region = sys.argv[1]
 
-# Load Azure app defaults
-try:
-   with open('azurermconfig.json') as configFile:    
-      configData = json.load(configFile)
-except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
-   sys.exit()
-   
-tenant_id = configData['tenantId']
-app_id = configData['appId']
-app_secret = configData['appSecret']
-subscription_id = configData['subscriptionId']
+    # Load Azure app defaults
+    try:
+        with open('azurermconfig.json') as config_file:
+            config_data = json.load(config_file)
+    except FileNotFoundError:
+        sys.exit('Error: Expecting azurermconfig.json in current folder')
 
-access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+    tenant_id = config_data['tenantId']
+    app_id = config_data['appId']
+    app_secret = config_data['appSecret']
+    sub_id = config_data['subscriptionId']
 
-# print quota
-if region == 'all':
-    # list locations
-    locations = azurerm.list_locations(access_token, subscription_id)
-    for location in locations['value']:
-        print_region_quota(location['name'])
-else:
-    print_region_quota(region)
+    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
+    # print quota
+    if region == 'all':
+        # list locations
+        locations = azurerm.list_locations(access_token, sub_id)
+        for location in locations['value']:
+            print_region_quota(access_token, sub_id, location['name'])
+    else:
+        print_region_quota(access_token, sub_id, region)
+
+
+if __name__ == "__main__":
+    main()
--- examples/list_rgs.py
+++ examples/list_rgs.py
@@ -1,29 +1,34 @@
-import azurerm
+'''list_rgs.py - list Azure resource groups in a subscription'''
 import json
+import sys
 
-# Load Azure app defaults
-try:
-   with open('azurermconfig.json') as configFile:    
-      configData = json.load(configFile)
-except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
-   sys.exit()
-   
-tenant_id = configData['tenantId']
-app_id = configData['appId']
-app_secret = configData['appSecret']
-subscription_id = configData['subscriptionId']
+import azurerm
 
-access_token = azurerm.get_access_token(
-    tenant_id,
-    app_id,
-    app_secret
-)
+def main():
+    '''Main routine.'''
+    # Load Azure app defaults
+    try:
+        with open('azurermconfig.json') as config_file:
+            config_data = json.load(config_file)
+    except FileNotFoundError:
+        sys.exit("Error: Expecting vmssConfig.json in current folder")
 
-# list resource groups
-resource_groups = azurerm.list_resource_groups(access_token, subscription_id)
-for rg in resource_groups['value']:
-    print(rg['name'] + ', ' + rg['location'] + ', ' + rg['properties']['provisioningState'])
-    print('Resource group details..')
-    rg_details = azurerm.get_resource_group(access_token, subscription_id, rg['name'])
-    print(json.dumps(rg_details, sort_keys=False, indent=2, separators=(',', ': ')))
+    tenant_id = config_data['tenantId']
+    app_id = config_data['appId']
+    app_secret = config_data['appSecret']
+    sub_id = config_data['subscriptionId']
+
+    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+
+    # list resource groups
+    resource_groups = azurerm.list_resource_groups(access_token, sub_id)
+    for rgname in resource_groups['value']:
+        print(rgname['name'] + ', ' + rgname['location'] + ', '
+              + rgname['properties']['provisioningState'])
+        print('Resource group details..')
+        rg_details = azurerm.get_resource_group(access_token, sub_id, rgname['name'])
+        print(json.dumps(rg_details, sort_keys=False, indent=2, separators=(',', ': ')))
+
+
+if __name__ == "__main__":
+    main()
