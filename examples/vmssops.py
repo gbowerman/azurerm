c67791fd7aea9a1f4843cacffaf6a94086b8f667
Guy Bowerman <guybo@outlook.com>
2017-08-27 22:43:26 -0700

    Pylint cleanup work on example programs

--- examples/vmssops.py
+++ examples/vmssops.py
@@ -1,39 +1,52 @@
-import azurerm
+'''vmssops.py - example program to show running operations on scale set VMs'''
 import json
+import sys
 
-# Load Azure app defaults
-try:
-   with open('azurermconfig.json') as configFile:    
-      configData = json.load(configFile)
-except FileNotFoundError:
-   print("Error: Expecting vmssConfig.json in current folder")
-   sys.exit()
-   
-tenant_id = configData['tenantId']
-app_id = configData['appId']
-app_secret = configData['appSecret']
-subscription_id = configData['subscriptionId']
-resource_group = configData['resourceGroup']
-vmssname = configData['vmssName']
+import azurerm
 
-access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
-# delete vmss vm id #1
-#vm_ids = '["1"]'
-#result = azurerm.delete_vmss_vms(access_token, subscription_id, resource_group, vmssname, vm_ids)
-#print(result)
+def main():
+    '''main routine'''
+    # Load Azure app defaults
+    try:
+        with open('azurermconfig.json') as config_file:
+            config_data = json.load(config_file)
+    except FileNotFoundError:
+        sys.exit('Error: Expecting azurermconfig.json in current folder')
 
-# restart vmss vm id's #2, #3
-#vm_ids = '["2", "3"]'
-#result = azurerm.restart_vmss_vms(access_token, subscription_id, resource_group, vmssname, vm_ids)
-#print(result)
+    tenant_id = config_data['tenantId']
+    app_id = config_data['appId']
+    app_secret = config_data['appSecret']
+    subscription_id = config_data['subscriptionId']
+    resource_group = config_data['resourceGroup']
+    vmssname = config_data['vmssName']
 
-# poweroff some vmss vm's
-vm_ids = '["7", "9"]'
-result = azurerm.poweroff_vmss_vms(access_token, subscription_id, resource_group, vmssname, vm_ids)
-print(result)
+    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
-# start vmss vm id's #2, #3
-#vm_ids = '["2", "3"]'
-#result = azurerm.start_vmss_vms(access_token, subscription_id, resource_group, vmssname, vm_ids)
-#print(result)
+    # delete vmss vm id #1
+    #vm_ids = '["1"]'
+    #result = azurerm.delete_vmss_vms(access_token, subscription_id, resource_group, vmssname,
+    #                                 vm_ids)
+    # print(result)
+
+    # restart vmss vm id's #2, #3
+    #vm_ids = '["2", "3"]'
+    #result = azurerm.restart_vmss_vms(access_token, subscription_id, resource_group, vmssname,
+    #                                  vm_ids)
+    # print(result)
+
+    # poweroff some vmss vm's
+    vm_ids = '["7", "9"]'
+    result = azurerm.poweroff_vmss_vms(access_token, subscription_id, resource_group, vmssname,
+                                       vm_ids)
+    print(result)
+
+    # start vmss vm id's #2, #3
+    #vm_ids = '["2", "3"]'
+    #result = azurerm.start_vmss_vms(access_token, subscription_id, resource_group, vmssname,
+    #                                vm_ids)
+    # print(result)
+
+
+if __name__ == '__main__':
+    main()
