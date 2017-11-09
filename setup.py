6f267a03ddd119e5785254f23fd7d0eae5b3b3dd
Guy Bowerman <guybo@outlook.com>
2017-11-09 08:45:01 -0800

    Bump Compute API version to 2017-12-01

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 
 setup(name='azurerm',
-      version='0.9.4',
+      version='0.9.5',
       description='Azure Resource Manager REST wrappers',
       long_description=LONG_DESCRIPTION,
       url='http://github.com/gbowerman/azurerm',
