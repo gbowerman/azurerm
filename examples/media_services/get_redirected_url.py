babeb27259d4f3e81b4a97e26c1a5a3261e82800
Marcelo Leal <msl@eall.com.br>
2017-11-27 14:40:11 -0200

    Azure Media Services Full Examples...

---
+++ examples/media_services/get_redirected_url.py
@@ -0,0 +1,55 @@
+"""
+Copyright (c) 2016, Marcelo Leal
+Description: Simple Azure Media Services Python library
+License: MIT (see LICENSE.txt file for details)
+"""
+import os
+import json
+import azurerm
+import time
+#import pytz
+import logging
+import datetime
+
+###########################################################################################
+##### DISCLAIMER ##### ##### DISCLAIMER ##### ##### DISCLAIMER ##### ##### DISCLAIMER #####
+###########################################################################################
+
+# ALL CODE IN THIS DIRECTOY (INCLUDING THIS FILE) ARE EXAMPLE CODES THAT  WILL  ACT ON YOUR
+# AMS ACCOUNT.  IT ASSUMES THAT THE AMS ACCOUNT IS CLEAN (e.g.: BRAND NEW), WITH NO DATA OR
+# PRODUCTION CODE ON IT.  DO NOT, AGAIN: DO NOT RUN ANY EXAMPLE CODE AGAINST PRODUCTION AMS
+# ACCOUNT!  IF YOU RUN ANY EXAMPLE CODE AGAINST YOUR PRODUCTION  AMS ACCOUNT,  YOU CAN LOSE
+# DATA, AND/OR PUT YOUR AMS SERVICES IN A DEGRADED OR UNAVAILABLE STATE. BE WARNED!
+
+###########################################################################################
+##### DISCLAIMER ##### ##### DISCLAIMER ##### ##### DISCLAIMER ##### ##### DISCLAIMER #####
+###########################################################################################
+
+# Load Azure app defaults
+try:
+	with open('config.json') as configFile:
+		configData = json.load(configFile)
+except FileNotFoundError:
+	print("ERROR: Expecting config.json in current folder")
+	sys.exit()
+
+account_name = configData['accountName']
+account_key = configData['accountKey']
+
+# Get the access token...
+response = azurerm.get_ams_access_token(account_name, account_key)
+resjson = response.json()
+access_token = resjson["access_token"]
+
+#Initialization...
+print ("\n-----------------------= AMS Py =----------------------");
+print ("Simple Python Library for Azure Media Services REST API");
+print ("-------------------------------------------------------\n");
+
+### get ams redirected url
+response = azurerm.get_url(access_token)
+if (response.status_code == 200):
+	print("New Redirected URL: " + str(response.url))
+else:
+	print("GET Status: " + str(response.status_code) + " - Getting Redirected URL ERROR." + str(response.content))
+
