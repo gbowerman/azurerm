3670bff0000f9b98fce9a47c29b8adde94ae5716
Guy Bowerman <guybo@outlook.com>
2018-11-14 16:29:57 -0800

    Update delete_rgs.py and list_rgs.py to get auth token from MSI endpoint in cloud shell

--- examples/delete_rg.py
+++ examples/delete_rg.py
@@ -1,36 +1,37 @@
 '''delete_rg.py - example script to delete an Azure Resource Group'''
 import json
+import os
 import sys
 
-import azurerm
-
-
-def usage():
-    '''Basic usage function to print usage and exit.'''
-    sys.exit('Usage: python ' + sys.argv[0] + ' rg_name')
+import azurerm    
 
 
 def main():
     '''Main routine.'''
     # check for single command argument
     if len(sys.argv) != 2:
-        usage()
+        sys.exit('Usage: python ' + sys.argv[0] + ' rg_name')
 
     rgname = sys.argv[1]
 
-    # Load Azure app defaults
-    try:
-        with open('azurermconfig.json') as config_file:
-            config_data = json.load(config_file)
-    except FileNotFoundError:
-        sys.exit('Error: Expecting azurermconfig.json in current folder')
+    # if in Azure cloud shell, authenticate using the MSI endpoint
+    if 'ACC_CLOUD' in os.environ and 'MSI_ENDPOINT' in os.environ:
+        access_token = azurerm.get_access_token_from_cli()
+        subscription_id = azurerm.get_subscription_from_cli()
+    else: # load service principal details from a config file        
+        try:
+            with open('azurermconfig.json') as configfile:
+                configdata = json.load(configfile)
+        except FileNotFoundError:
+            sys.exit('Error: Expecting azurermconfig.json in current folder')
 
-    tenant_id = config_data['tenantId']
-    app_id = config_data['appId']
-    app_secret = config_data['appSecret']
-    subscription_id = config_data['subscriptionId']
+        tenant_id = configdata['tenantId']
+        app_id = configdata['appId']
+        app_secret = configdata['appSecret']
+        subscription_id = configdata['subscriptionId']
 
-    access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
+        # authenticate
+        access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
     # delete a resource group
     rgreturn = azurerm.delete_resource_group(access_token, subscription_id, rgname)
